65bea59682fd558990f0436cfda20367cf6b9328
divisoryang <clr.developer@gmail.com>
2012-09-23 13:09:21 +0800

    build  wikish project

---
+++ Wikish/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Wikish
+//
+//  Created by YANG ENZO on 12-9-23.
+//  Copyright (c) 2012年 Side Trip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Wikish/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Wikish
+//
+//  Created by YANG ENZO on 12-9-23.
+//  Copyright (c) 2012年 Side Trip. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
